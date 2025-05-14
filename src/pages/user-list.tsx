61b91587ca5c77413b8247eb89d1f993a25e97ee
juliacribeiro <juliacrmg@gmail.com>
2025-05-14 09:01:47 -0300

    Shows users info on user-list page

--- src/pages/user-list.tsx
+++ src/pages/user-list.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UserCard } from "../components/users/user-card";
+
+interface User {
+  name: string;
+  email: string;
+}
 
 export const UserList = () => {
-    return (
-        <></>
-    );
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch("/user-data.json")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      {users.map((user, index) => (
+        <UserCard key={index} userEmail={user.email} userName={user.name} />
+      ))}
+    </div>
+  );
 };
