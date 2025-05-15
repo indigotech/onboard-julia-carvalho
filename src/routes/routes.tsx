b5de2150ace22d9ac117d2692809e6fdf032cd63
juliacribeiro <juliacrmg@gmail.com>
2025-05-15 15:41:17 -0300

    Adds create-user screen and a create button on user-list

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "../pages/login";
 import { UserList } from "../pages/user-list";
+import { CreateUser } from "../pages/create-user";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/user-list" element={<UserList />} />
+      <Route path="/create-user" element={<CreateUser />} />
     </Routes>
   );
 };
--- src/utils/validation.tsx
+++ src/utils/validation.tsx
@@ -1,5 +1,8 @@
 const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.com(\.br)?$/;
 const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d).{7,}$/;
+const NAME_REGEX = /^[A-Za-zÀ-ÿ]+(?:\s+[A-Za-zÀ-ÿ]+)+$/;
+const PHONE_REGEX = /^\d{10,11}$/;
+const ROLE_REGEX = /^(user|admin)$/;
 
 export const isValidEmail = (email: string): boolean => {
   return EMAIL_REGEX.test(email);
@@ -7,4 +10,24 @@ export const isValidEmail = (email: string): boolean => {
 
 export const isValidPassword = (password: string): boolean => {
   return PASSWORD_REGEX.test(password);
+};
+
+export const isValidName = (name: string): boolean => {
+  return NAME_REGEX.test(name);
+};
+
+export const isValidPhone = (phone: string): boolean => {
+  return PHONE_REGEX.test(phone);
+};
+
+export const isValidRole = (role: string): boolean => {
+  return ROLE_REGEX.test(role);
+};
+
+export const isValidBirthDate = (dateStr: Date) => {
+  const date = new Date(dateStr);
+  const today = new Date();
+  const minDate = new Date("1900-01-01");
+
+  return !isNaN(date.getTime()) && date <= today && date >= minDate;
 };
