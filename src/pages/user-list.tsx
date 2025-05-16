e854c8df54e39d8fd4de8b0172c7b189d5d406c9
juliacribeiro <juliacrmg@gmail.com>
2025-05-15 22:19:12 -0300

    Fixes problem where the user can click on create-user button without being logged in

--- src/pages/user-list.tsx
+++ src/pages/user-list.tsx
@@ -76,14 +76,19 @@ export const UserList = () => {
 
   return (
     <div>
-      <div style={HeaderStyle}>
-        <div style={HeaderTitleStyle}> Lista de Usuários </div>
-        <CustomButton title="Criar um novo usuário" onClick={() => navigate("/create-user")} />
-      </div>
-      <div>
-        {error && <p>{error}</p>}
-        {!error &&
-          users.map((user) => (
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          <div style={HeaderStyle}>
+            <div style={HeaderTitleStyle}>Lista de Usuários</div>
+            <CustomButton
+              title="Criar um novo usuário"
+              onClick={() => navigate("/create-user")}
+            />
+          </div>
+
+          {users.map((user) => (
             <UserCard
               key={user.id}
               userEmail={user.email}
@@ -91,17 +96,14 @@ export const UserList = () => {
             />
           ))}
 
-        {!error && (
-          <>
-            <InfiniteScroll
-              loadMore={fetchUsers}
-              hasMore={hasMore}
-              loading={loading}
-            />
-            {loading && <p>Carregando usuários...</p>}
-          </>
-        )}
-      </div>
+          <InfiniteScroll
+            loadMore={fetchUsers}
+            hasMore={hasMore}
+            loading={loading}
+          />
+          {loading && <p>Carregando usuários...</p>}
+        </>
+      )}
     </div>
   );
 };
